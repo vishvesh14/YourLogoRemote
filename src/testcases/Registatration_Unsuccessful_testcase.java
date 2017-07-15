198b4e754537aadef06ddf3242e75820492c74e4
Vishvesh14 <vishveshsavant@gmail.com>
2017-07-15 20:42:51 +0530

    Provided required comments

--- src/testcases/Registatration_Unsuccessful_testcase.java
+++ src/testcases/Registatration_Unsuccessful_testcase.java
@@ -10,10 +10,14 @@ public class Registatration_Unsuccessful_testcase extends Base{
 
 	@Test
 	public void Registration_Unsuccessful() {
+//Object Created for Class Registration
 		Registration regobj = new Registration(driver);
+		
+//Expected Error Message
 		String expected ="Invalid email address";
 		
 		try {
+//Calling Methods from Registration Class
 			regobj.ClickSignIn();
 			regobj.ClickEmail();
 
@@ -21,17 +25,23 @@ public class Registatration_Unsuccessful_testcase extends Base{
 			ExcelWBook = new XSSFWorkbook(ExcelFile);
 			ExcelWSheet = ExcelWBook.getSheet(sheetName);
 			
+//Getting the Excel File Row count
 			int row_count = ExcelWSheet.getLastRowNum();
 			
+//Looping through the row
 			for(int i=1;i<=row_count;i++){
+//Getting data from Rows
 				Cell = ExcelWSheet.getRow(i).getCell(0);
 				String celldata = Cell.getStringCellValue();
 				regobj.EnterEmail(celldata);
+//Calling method from Registration
 				regobj.ClickCreateAccountButton();
 				Thread.sleep(5000);
 				
+//Checking the Div that has the error message
 				boolean create_account_error_div = driver.findElement(By.id("create_account_error")).isDisplayed();
 				System.out.println(create_account_error_div);
+//Getting Actual Error Message
 				String actual = driver.findElement(By.id("create_account_error")).getText();
 				while(create_account_error_div == true){
 					if(expected.equals(actual)){
